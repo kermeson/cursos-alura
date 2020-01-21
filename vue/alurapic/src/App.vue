c728d9a87fcd12182ed1c03aca571e75b6ffef14
Kermeson Martins <kermeson@gmail.com>
2020-01-21 16:49:04 -0300

    curso vue

---
+++ vue/alurapic/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>{{ name }}</h1>
+    <ul>
+      <li v-for="foto of fotos" :key="foto.titulo">
+        <img :src="foto.url" :alt="foto.titulo" />
+      </li>
+    </ul>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      name: 'Projeto AluraPicure',
+      fotos: [
+        {
+          url: 'https://ichef.bbci.co.uk/news/410/cpsprodpb/6F51/production/_101779482_gettyimages-654134840.jpg',
+          titulo: 'Foto 1'
+        },
+        {
+          url: 'https://ichef.bbci.co.uk/news/410/cpsprodpb/6F51/production/_101779482_gettyimages-654134840.jpg',
+          titulo: 'Foto 2'
+        }
+      ] 
+    }
+  }
+}
+</script>
+
+<style>
+</style>
