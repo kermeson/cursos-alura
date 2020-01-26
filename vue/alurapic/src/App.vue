1e4b05513c4c09d7436e5fe809f1599ddf68d082
Kermeson Martins <kermeson@gmail.com>
2020-01-26 15:09:08 -0300

    trabalhando com api e vue

--- vue/alurapic/src/App.vue
+++ vue/alurapic/src/App.vue
@@ -1,35 +1,64 @@
 <template>
-  <div>
-    <h1>{{ name }}</h1>
-    <ul>
-      <li v-for="foto of fotos" :key="foto.titulo">
-        <img :src="foto.url" :alt="foto.titulo" />
+  <div class="corpo">
+    <h1 class="titulo">{{ name }}</h1>
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotos" :key="foto.titulo">
+        <meu-painel :titulo="foto.titulo">
+          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo" />
+        </meu-painel>
       </li>
     </ul>
   </div>
-  
 </template>
 
 <script>
+import Painel from "./components/shared/Painel";
+
 export default {
-  name: 'app',
-  data () {
+  name: "app",
+  components: {
+    "meu-painel": Painel
+  },
+  data() {
     return {
-      name: 'Projeto AluraPicure',
-      fotos: [
-        {
-          url: 'https://ichef.bbci.co.uk/news/410/cpsprodpb/6F51/production/_101779482_gettyimages-654134840.jpg',
-          titulo: 'Foto 1'
-        },
-        {
-          url: 'https://ichef.bbci.co.uk/news/410/cpsprodpb/6F51/production/_101779482_gettyimages-654134840.jpg',
-          titulo: 'Foto 2'
-        }
-      ] 
-    }
+      name: "Projeto AluraPicure",
+      fotos: []
+    };
+  },
+  created() {
+    this.$http
+      .get("http://localhost:3000/v1/fotos")
+      .then(res => res.json())
+      .then(
+        res => (this.fotos = res),
+        err => console.log(err)
+      );
   }
-}
+};
 </script>
 
 <style>
+
+  .titulo {
+    text-align: center;
+  }
+
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+
+  .imagem-responsiva {
+    width: 100%;
+  }
+
 </style>
